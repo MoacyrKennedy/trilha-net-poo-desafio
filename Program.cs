b1046ddbfc4045f7e4019f34f80e9266d997d1f3
Moacyr kennedy <105392455+MoacyrKennedy@users.noreply.github.com>
2023-12-19 10:43:26 -0300

    Desafio Completo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone iphone:");
+Iphone iphone = new Iphone(numero: "1", modelo: "A", imei: "1",memoria: 32);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Twicth");
+
+Console.WriteLine("----------------------------------------------------------");
+
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia(numero: "2", modelo: "B", imei: "2",memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Youtube");
+
+Console.WriteLine("-----------------------------------------------------------");
